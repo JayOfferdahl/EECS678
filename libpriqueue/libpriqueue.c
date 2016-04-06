8d6a68325420722b745cc18e7c5a3460cf3f1ee1
Jay Offerdahl <jaysanco@gmail.com>
2016-04-05 20:04:18 -0500

    Added priqueue functions back

--- libpriqueue/libpriqueue.c
+++ libpriqueue/libpriqueue.c
@@ -91,6 +91,90 @@ int priqueue_offer(priqueue_t *q, void *ptr)
 
 
 /**
+  Retrieves, but does not remove, the head of this queue, returning NULL if
+  this queue is empty.
+ 
+  @param q a pointer to an instance of the priqueue_t data structure
+  @return pointer to element at the head of the queue
+  @return NULL if the queue is empty
+ */
+void *priqueue_peek(priqueue_t *q)
+{
+  return NULL;
+}
+
+
+/**
+  Retrieves and removes the head of this queue, or NULL if this queue
+  is empty.
+ 
+  @param q a pointer to an instance of the priqueue_t data structure
+  @return the head of this queue
+  @return NULL if this queue is empty
+ */
+void *priqueue_poll(priqueue_t *q)
+{
+  return NULL;
+}
+
+
+/**
+  Returns the element at the specified position in this list, or NULL if
+  the queue does not contain an index'th element.
+ 
+  @param q a pointer to an instance of the priqueue_t data structure
+  @param index position of retrieved element
+  @return the index'th element in the queue
+  @return NULL if the queue does not contain the index'th element
+ */
+void *priqueue_at(priqueue_t *q, int index)
+{
+  return NULL;
+}
+
+
+/**
+  Removes all instances of ptr from the queue. 
+  
+  This function should not use the comparer function, but check if the data contained in each element of the queue is equal (==) to ptr.
+ 
+  @param q a pointer to an instance of the priqueue_t data structure
+  @param ptr address of element to be removed
+  @return the number of entries removed
+ */
+int priqueue_remove(priqueue_t *q, void *ptr)
+{
+  return 0;
+}
+
+
+/**
+  Removes the specified index from the queue, moving later elements up
+  a spot in the queue to fill the gap.
+ 
+  @param q a pointer to an instance of the priqueue_t data structure
+  @param index position of element to be removed
+  @return the element removed from the queue
+  @return NULL if the specified index does not exist
+ */
+void *priqueue_remove_at(priqueue_t *q, int index)
+{
+  return 0;
+}
+
+
+/**
+  Destroys and frees all the memory associated with q.
+  
+  @param q a pointer to an instance of the priqueue_t data structure
+ */
+void priqueue_destroy(priqueue_t *q)
+{
+
+}
+
+
+/**
   Returns the number of elements in the queue.
  
   @param q a pointer to an instance of the priqueue_t data structure

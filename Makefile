df1e01bd37193a5edb3826974c24e758c020f8db
jayofferdahl <jaysanco@gmail.com>
2016-05-02 17:15:39 -0500

    Finish buddy_alloc().

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ CFLAGS = -Wall -g
 ####################################################################
 # NOTE: The submission scripts assume all files in `CFILES` end with
 # .c and all files in `HFILES` end in .h
-CFILES = simulator.c buddy.c
+CFILES = buddy.c simulator.c
 HFILES = buddy.h list.h
 
 # Add libraries that need linked as needed (e.g. -lm -lpthread)
--- buddy.c
+++ buddy.c
@@ -51,9 +51,14 @@
 typedef struct {
 	struct list_head list;
 
-	/* TODO: DECLARE NECESSARY MEMBER VARIABLES */
+	// Integer to hold the order size the current block
 	int block_size_order;
-	int occupied;
+
+	// The index of this page
+	int index;
+
+	// The memory stored at this page
+	char *mem;
 } page_t;
 
 /**************************************************************************
@@ -83,17 +88,17 @@ void buddy_init()
 {
 	int i;
 	int n_pages = (1<<MAX_ORDER) / PAGE_SIZE;
-	for (i = 0; i < n_pages; i++) {
-		/* TODO: INITIALIZE PAGE STRUCTURES */
 
+	for (i = 0; i < n_pages; i++) {
 		// Initialize the page list
 		INIT_LIST_HEAD(&g_pages[i].list);
-		g_pages[i].block_size_order = -1;
-		g_pages[i].occupied = 0;
 
-		if(i != 0)
-			list_add(&g_pages[i].list, &g_pages[0].list);
+		// Initialize the page variables
+		g_pages[i].block_size_order = -1;
+		g_pages[i].index = i;
+		g_pages[i].mem = PAGE_TO_ADDR(i);
 	}
+	// To begin, it's all one block
 	g_pages[0].block_size_order = MAX_ORDER;
 
 	/* initialize freelist */
@@ -121,57 +126,78 @@ void buddy_init()
  */
 void *buddy_alloc(int size)
 {
+	// Check if out of bounds
+	if(size > (1 << MAX_ORDER)) {
+		return NULL;
+	}
+
 	// Determine what order is needed to allocate this memory
 	int size_order = 1,
 		request_size = size,
-		request_page,
-		request_page_length;
+		i;
 
 	while((request_size /= 2) > 0)
 		size_order++;
+
 
 #if USE_DEBUG
 	printf("Requested size is %i, order of %i\n", size, size_order);
 #endif
 
-	// Find the next available memory
-	for(int i = size_order; i < MAX_ORDER; i++) {
+	// Find the next available memory, if we are at a free block that's the correct
+	// order, we'll use that block. If not, we'll recursively break it down
+	for(i = size_order; i <= MAX_ORDER; i++) {
 
 		// If there's an available block, begin partitioning it
-		if(!list_empty(free_area[i])) {
+		if(!list_empty(&free_area[i])) {
 
 		#if USE_DEBUG
 			printf("Found empty block of order %i, partitioning...\n", i);
 		#endif
 
+			page_t *left, *right;
+			int request_page_index;
+			void *request_page_addr;
+
 			// Break this block down until we have an appropriate size. Once 
 			// we've determined we have a block big enough, return it's address
-			for(int j = i; j >= size_order; j--) {
-				request_page_length = (1 << j) / PAGE_SIZE;
-				request_page = find_free_block_page(j); // TODO: make this function which returns the first index of free memory of this size
+			// If we're at the smallest block to allocate the request, return the address
+			if(i == size_order) {
+			#if USE_DEBUG
+				printf("Block of order %i, has been partitioned, returning address...\n", i);
+			#endif
+				// Grab onto the left side of this list
+				left = list_entry(free_area[i].next, page_t, list);
 
-				// If we're at the smallest block to allocate the request, return the address
-				if(j == size_order) {
-					void *request_addr = PAGE_TO_ADDR(request_page);
-
-				#if USE_DEBUG
-					printf("Marking pages %i through %i as occupied...\n", request_page, request_page + request_page_length);
-				#endif
-
-					// Mark each page as occupied
-					while(request_page_length > 0)
-						g_pages[request_page + request_page_length--].occupied = 1;
-
-					return request_addr;
-				}
-				// Else, break the block down and appropriately add half to free_area
-				else {
-					// Break the list into two halves
-					
-					// Add the right block to free_area
-					list_add(g_pages[request_page + request_page_length].list, free_area[j]);
-				}
+				// Delete this entry
+				list_del(&(left->list));
 			}
+			// Else, break the block down and appropriately add half to free_area
+			else {
+				// Recursively grab onto the most left page of the breakdown
+				#if USE_DEBUG
+					printf("Entering recursion...\n");
+				#endif
+				left = &g_pages[ADDR_TO_PAGE(buddy_alloc((1 << (size_order))))];
+
+			#if USE_DEBUG
+				if(left == NULL)
+					printf("Recursive call to buddy_alloc returned NULL...please evaluate\n");
+			#endif
+				// Calculate the block's index to the right of left
+				request_page_index = left->index + (1 << size_order) / PAGE_SIZE;
+				right = &g_pages[request_page_index];
+
+				// Add the right list to free area
+				list_add(&(right->list), &free_area[size_order]);
+			}
+
+			// Recursion finished (At this step)...update left block properties
+			left->block_size_order = size_order;
+
+			// Calculate the requested page's address and return it
+			request_page_addr = PAGE_TO_ADDR(left->index);
+			return request_page_addr;
 		}
 	}
 
@@ -211,3 +237,39 @@ void buddy_dump()
 	}
 	printf("\n");
 }
+
+/*
+int main(int argc, char *argv[])
+{
+	char *A, *B, *C, *D;
+
+	buddy_init();
+
+	printf("Buddy initialized...\n");
+ 	
+	A = buddy_alloc(80*1024);
+	buddy_dump();
+
+	B = buddy_alloc(60*1024);
+	buddy_dump();
+
+	C = buddy_alloc(80*1024);
+	buddy_dump();
+
+	buddy_free(A);
+	buddy_dump();
+
+	D = buddy_alloc(32*1024);
+	buddy_dump();
+	
+	buddy_free(B);
+	buddy_dump();
+	
+	buddy_free(D);
+	buddy_dump();
+
+	buddy_free(C);
+	buddy_dump();
+
+	return 0;
+}*/
